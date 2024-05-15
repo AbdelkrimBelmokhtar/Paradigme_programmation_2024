7e12831d3daff2e28956e1c567fd3ba8b8ed0723
Abdelkrim <abdelkrim.pro@outlook.fr>
2024-05-15 17:02:53 +0200

    controle

--- Java/src/Exercice1/Membre.java
+++ Java/src/Exercice1/Membre.java
@@ -1,5 +1,19 @@
 package Exercice1;
 
-public class Membre {
+public class Membre extends Personne {
 
+	public String dateAdhesion;
+	public String statut;
+
+	public Membre(String nom, String prenom, int id, String dateAdhesion, String statut) {
+		super(nom, prenom, id);
+	}
+
+public void afficherDetails() {
+		System.out.println("Le nom du membre est " + this.nom);
+		System.out.println("Le prenom du membre est " + this.prenom);
+		System.out.println("L'id de la personne est " + this.id);
+		System.out.println("La date de l'adhésion du membre est " + this.dateAdhesion);
+		System.out.println("Le statut du membre est " + this.statut);
+}
 }
